62009b7bb787aac185d3cf958c1b14bc0db9695e
jfct <jorgecosteixeira@gmail.com>
2022-07-13 01:38:29 +0100

    feat: changed main server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { LogLevel, SapphireClient } from '@sapphire/framework';
+import '@sapphire/plugin-editable-commands/register';
 import '@sapphire/plugin-logger/register';
+import path from 'path';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 import './lib/setup';
+import { Command } from './models/command.model';
+import { Genre } from './models/genre.model';
+import { Request } from './models/request.model';
+import { Song } from './models/song.model';
+import { User } from './models/user.model';
 import { VoiceManagerGroup } from './types/voice.types';
 
 const client = new SapphireClient({
@@ -10,6 +19,7 @@ const client = new SapphireClient({
 	logger: {
 		level: LogLevel.Debug
 	},
+	loadMessageCommandListeners: true,
 	shards: 'auto',
 	intents: [
 		'GUILDS',
@@ -26,15 +36,47 @@ const client = new SapphireClient({
 
 export const voiceConnections: VoiceManagerGroup[] = [];
 
+export const AppDataSource = new DataSource({
+	type: 'sqlite',
+	database: path.join(process.cwd(), 'discord.db'),
+	synchronize: true,
+	entities: [
+		Command,
+		Genre,
+		Request,
+		Song,
+		User
+	],
+	logging: 'all'
+});
+
 process.on('exit', () => {
+	console.log('\nGracefully shutting down client');
 	client.destroy();
+});
+
+process.on('SIGINT', function () {
+	console.log('\nGracefully shutting down from SIGINT (Ctrl-C)');
+	// some other closing procedures go here
+	process.exit(0);
 });
 
 const main = async () => {
 	try {
+		// Initialize Client (sapphire)
 		client.logger.info('Logging in');
 		await client.login(process.env.DISCORD_TOKEN);
 		client.logger.info('logged in');
+
+		// Initialize DB connection
+		AppDataSource.initialize()
+			.then(() => {
+				//const db: Database = Database.build();
+				//await db.close();
+			})
+			.catch((err) => {
+				console.log('error! => ', err);
+			});
 	} catch (error) {
 		client.logger.fatal(error);
 		client.destroy();
