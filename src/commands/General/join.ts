aa984c4d1d2223930bf8766fe7bd96aede0d1c32
jfct <jorgecosteixeira@gmail.com>
2022-11-08 12:14:03 +0000

    feat: added the join and leave command

---
+++ src/commands/General/join.ts
@@ -0,0 +1,32 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { ChatInputCommand, Command, CommandOptions, RegisterBehavior } from '@sapphire/framework';
+import { VoiceChannel } from 'discord.js';
+import { Voice } from '../../lib/Voice';
+
+@ApplyOptions<CommandOptions>({
+	description: 'Joins the voice channel'
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) => builder.setName(this.name).setDescription(this.description), {
+			behaviorWhenNotIdentical: RegisterBehavior.Overwrite
+		});
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputInteraction) {
+		// Checking if the message author is a bot.
+		if (interaction.user.bot) return false;
+		// Checking if the message was sent in a DM channel.
+		if (!interaction.guild) return false;
+
+		const guild = await interaction.guild?.fetch();
+		const member = await guild?.members.fetch(interaction.user.id);
+		const voiceChannel = member?.voice.channel;
+
+		if (Boolean(voiceChannel) && voiceChannel instanceof VoiceChannel) {
+			await Voice.join(voiceChannel);
+		}
+
+		return interaction.reply({ ephemeral: true, content: 'Já vou' });
+	}
+}
